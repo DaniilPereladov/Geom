a63c5b438303a4d85f6a492f39b3c97e1a8754f7
JoraPiton <perelad2003@yandex.ru>
2021-09-14 16:23:05 +0700

    Shape Draw IDraw

---
+++ src/lib/CoreLib/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib
+{
+   public struct Point :IDraw
+    {
+        private const char Symbol = 'X';
+        public int X, Y;
+
+        public Point(int x, int y) 
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Draw()
+        {
+            SetCursorPosition(X,Y);
+            Write(Symbol);
+        }
+    }
+}
---
+++ src/lib/CoreLib/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib
+{
+    public abstract class Shape : IDraw
+    {
+        protected virtual IList<Point> Locus { get; init; } = new List<Point>();
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
