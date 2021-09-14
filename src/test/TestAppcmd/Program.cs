f12c72b4de918db3a26c27c9160fa7aec5c1d5f7
JoraPiton <perelad2003@yandex.ru>
2021-09-14 15:42:42 +0700

    Geom готов

---
+++ src/test/TestAppcmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestAppcmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
