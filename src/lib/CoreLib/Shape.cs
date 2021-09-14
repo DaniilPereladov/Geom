ce60ccf5f99075eb2d6d7b0a2a3e46fadc30187a
JoraPiton <perelad2003@yandex.ru>
2021-09-14 17:02:26 +0700

    IEnumerable

--- src/lib/CoreLib/Shape.cs
+++ src/lib/CoreLib/Shape.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,19 @@ using System.Threading.Tasks;
 
 namespace CoreLib
 {
-    public abstract class Shape : IDraw
+    public abstract class Shape : IDraw, IEnumerable<Point>
     {
         protected virtual IList<Point> Locus { get; init; } = new List<Point>();
         public void Draw()
         {
             foreach (Point point in Locus) point.Draw();
         }
+
+        public IEnumerator<Point> GetEnumerator() => Locus.GetEnumerator();
+        
+
+        IEnumerator IEnumerable.GetEnumerator() => Locus.GetEnumerator();
+       
+        
     }
 }
--- src/lib/CoreLib/Shapes/Lines/VerticalLine.cs
+++ src/lib/CoreLib/Shapes/Lines/VerticalLine.cs
@@ -8,6 +8,9 @@ namespace CoreLib.Shapes.Lines
 {
     public class VerticalLine: Line
     {
-        
+        public VerticalLine(int x, int y1,int y2)
+        {
+            for (int i = y1; i <= y2; i++) Locus.Add(new(x, i));
+        }
     }
 }
